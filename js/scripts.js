c957d26d9fc2562b5bbfedd4420cdbbfcc86457c
noy1224 <ktc24a31g0008@edu.kyoto-tech.ac.jp>
2024-07-19 16:28:53 +0900

    a

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ window.addEventListener('DOMContentLoaded', () => {
     alert('自己紹介サイトへようこそ！');
     console.log('自己紹介サイトへようこそ！');
 });
+
+const menu = document.querySelector('.menu');
+menu.addEventListener('click', () => {
+    // メニューを開閉する処理を追加する
+    // 例えば、クラスを切り替えて表示/非表示を制御するなど
+});
